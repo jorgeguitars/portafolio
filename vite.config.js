062ff72fb21e89cca5930936bda383134600977c
jorgemora <jorgeguitars@hotmail.com>
2024-09-21 14:27:55 -0500

    ziggy
    ziggy

--- vite.config.js
+++ vite.config.js
@@ -20,7 +20,7 @@ export default defineConfig({
     resolve: {
         alias: {
             '@': '/resources/js',
-            'ziggy': '/vendor/tightenco/ziggy/dist/vue.m', // Ajusta esta ruta según tu estructura
+           'ziggy': '/vendor/tightenco/ziggy/dist/vue.m',
         },
     },
 });
